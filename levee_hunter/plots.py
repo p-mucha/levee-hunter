18c810506b671f714d725dabbde4697dffafad92
Pawel Mucha <pawel_mucha@zohomail.eu>
2025-02-12 23:45:37 +0000

    Improved formatting and added new functio for plotting image and its target

--- levee_hunter/plots.py
+++ levee_hunter/plots.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
 
+
 def infer_and_visualize(model, image_tensor, mask_tensor, apply_sigmoid=True):
 
     if apply_sigmoid:
@@ -13,7 +14,7 @@ def infer_and_visualize(model, image_tensor, mask_tensor, apply_sigmoid=True):
     else:
         with torch.no_grad():
             output = model(image_tensor.unsqueeze(0))
-            output = np.array(output).reshape(256,256)
+            output = np.array(output).reshape(256, 256)
 
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 3, 1)
@@ -26,8 +27,43 @@ def infer_and_visualize(model, image_tensor, mask_tensor, apply_sigmoid=True):
 
     plt.subplot(1, 3, 3)
     plt.title("Target Mask")
-    plt.imshow(mask_tensor.reshape(256, 256), cmap=ListedColormap(["white", "black"]), vmin=0, vmax=1)
+    plt.imshow(
+        mask_tensor.reshape(256, 256),
+        cmap=ListedColormap(["white", "black"]),
+        vmin=0,
+        vmax=1,
+    )
 
     plt.show()
 
     return output
+
+
+def plot_img_and_target(img, target, figsize=(12, 6)):
+    if img.shape[0] == 1:
+        img = img.squeeze()
+
+    if target.shape[0] == 1:
+        target = target.squeeze()
+
+    fig, axes = plt.subplots(1, 2, figsize=figsize)  # 1 row, 2 columns
+
+    # Plot the first image
+    im = axes[0].imshow(img, cmap="viridis")
+    axes[0].set_title("Lidar Image")
+    axes[0].axis("off")
+
+    cbar = fig.colorbar(
+        im, ax=axes[0], orientation="vertical", fraction=0.046, pad=0.04
+    )
+    cbar.set_label("Value")
+
+    # Plot the second image
+    axes[1].imshow(
+        target, cmap=ListedColormap(["white", "black"]), vmin=0, vmax=1
+    )  # Explicitly map 0->white, 1->black
+    axes[1].set_title("Target Image")
+    axes[1].axis("off")
+
+    plt.tight_layout()
+    plt.show()
