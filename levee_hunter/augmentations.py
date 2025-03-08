117aa7bdcf06bec95ad5c32d9e43d48061812452
p-mucha <pawel_mucha@zohomail.eu>
2025-03-08 20:29:44 +0000

    Cleaned up augmentations, now segmentation_dataset will be able to access them through TRANSFORMS

--- levee_hunter/augmentations.py
+++ levee_hunter/augmentations.py
@@ -1,8 +1,8 @@
 import albumentations as A
 
 # Important Note:
-# When you add new transform, it also should be added to segmentation_dataset
-# in the __init__ method and in __perform_transform method.
+# When new transformation is added, it should be added to the TRANSFORMS dictionary below
+# There is no longer need to add it to __init__ of SegmentationDataset class
 
 
 # Define augmentations
@@ -17,7 +17,11 @@ train_transform = A.Compose(
         A.ElasticTransform(
             alpha=1, sigma=50, alpha_affine=50, p=0.5
         ),  # Elastic deformation
-        A.Lambda(image=lambda x, **kwargs: (x - x.mean()) / (x.std() + 1e-7))
+        A.Lambda(
+            name="z_score_normalize",
+            image=lambda x, **kwargs: (x - x.mean()) / (x.std() + 1e-7),
+        ),
+        # A.Lambda(image=lambda x, **kwargs: (x - x.min()) / (x.max() - x.min() + 1e-7)),
     ]
 )
 
@@ -26,20 +30,54 @@ no_deformations_transform = A.Compose(
         A.HorizontalFlip(p=0.5),  # Random horizontal flip
         A.VerticalFlip(p=0.5),  # Random vertical flip
         A.RandomRotate90(p=0.5),  # Random 90-degree rotation
-        A.Lambda(image=lambda x, **kwargs: (x - x.mean()) / (x.std() + 1e-7))
+        A.Lambda(
+            name="z_score_normalize",
+            image=lambda x, **kwargs: (x - x.mean()) / (x.std() + 1e-7),
+        ),
     ]
 )
 
 divide_by255_normalize = A.Compose(
     [
-        A.Normalize(mean=0.0, std=1.0), 
+        A.Normalize(mean=0.0, std=1.0),
     ]
 )
 
-z_score_normalize = A.Compose([
-    A.Lambda(image=lambda x, **kwargs: (x - x.mean()) / (x.std() + 1e-7))
-])
+z_score_normalize = A.Compose(
+    [
+        A.Lambda(
+            name="z_score_normalize",
+            image=lambda x, **kwargs: (x - x.mean()) / (x.std() + 1e-7),
+        )
+    ]
+)
 
-min_max_normalize = A.Compose([
-    A.Lambda(image=lambda x, **kwargs: (x - x.mean()) / (x.std() + 1e-7))
-])
+min_max_normalize = A.Compose(
+    [
+        A.Lambda(
+            name="min_max_normalize",
+            image=lambda x, **kwargs: (x - x.min()) / (x.max() - x.min() + 1e-7),
+        )
+    ]
+)
+
+normalize_only = A.Compose(
+    # [A.Lambda(image=lambda x, **kwargs: (x - x.min()) / (x.max() - x.min() + 1e-7))]
+    [
+        A.Lambda(
+            name="normalize_only",
+            image=lambda x, **kwargs: (x - x.mean()) / (x.std() + 1e-7),
+        )
+    ]
+)
+
+
+# Create a dictionary mapping names to transforms
+TRANSFORMS = {
+    "train_transform": train_transform,
+    "no_deformations_transform": no_deformations_transform,
+    "divide_by255_normalize": divide_by255_normalize,
+    "z_score_normalize": z_score_normalize,
+    "min_max_normalize": min_max_normalize,
+    "normalize_only": normalize_only,
+}
